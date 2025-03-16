eedb2a6a03e834431f4467a70d3ccef048588e56
Namaniitd <naman0865@gmail.com>
2025-03-16 15:48:41 +0900

    Initialize project

---
+++ src/main/java/com/online_chat/web_chat_system/WebChatSystemApplication.java
@@ -0,0 +1,13 @@
+package com.online_chat.web_chat_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChatSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/online_chat/web_chat_system/WebChatSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online_chat.web_chat_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
